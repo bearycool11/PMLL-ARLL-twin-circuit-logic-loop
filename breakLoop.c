2b9db607c1b163034fa11d7b6dbfbc0fe947241b
Josef Edwards <joed6834@colorado.edu>
2024-12-23 16:11:30 -0500

    Create breakLoop.c
    break free from the loops if needed

---
+++ breakLoop.c
@@ -0,0 +1,7 @@
+function breakLoop(reason?: string, pmll: PMLL, arll: ARLL, efll: EFLL): void {
+  // Add code here to break out of the loop
+  console.log("Breaking out of the loop");
+  pmll.reset();
+  arll.reset();
+  efll.reset();
+}
